70bfaae22980eaf6bacfef2b23184cb40ae03d64
lexvlncia <lexvalencia0001@gmail.com>
2026-05-18 07:12:14 +0800

    Update config.js

--- frontend/scripts/config.js
+++ frontend/scripts/config.js
@@ -1,6 +1,6 @@
 // configuration
 
-const link = "https://volume-proceeding-brothers-lime.trycloudflare.com" 
+const link = "https://volume-proceeding-brothers-lime.trycloudflare.com";
 export const API_BASE = link + ":5000/api";
 
 // links
